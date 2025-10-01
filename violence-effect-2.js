0ab9f2eb733c6a712247a7d5b04ed79ee391f7fc
meghansimm <meghan@meghans.im>
2025-09-30 21:04:27 -0500

    purered

--- violence-effect-2.js
+++ violence-effect-2.js
@@ -63,7 +63,7 @@ window.addEventListener('DOMContentLoaded', function() {
                     if (this.redTimer > 0) {
             this.redTimer--;
             if (this.redTimer === 0) {
-                this.color = "#F1271C";
+                this.color = "#FF0000";
                 this.hasTurnedRed = true;
             }
         }
